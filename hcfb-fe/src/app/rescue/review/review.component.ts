0cfecb9c113e792cd79c709b1d17f563733c8d88
Ash Wilson <smashwilson@github.com>
2019-09-17 11:22:55 -0400

    Format with prettier

--- hcfb-fe/src/app/rescue/review/review.component.ts
+++ hcfb-fe/src/app/rescue/review/review.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-review',
-  templateUrl: './review.component.html',
-  styleUrls: ['./review.component.scss']
+  selector: "app-review",
+  templateUrl: "./review.component.html",
+  styleUrls: ["./review.component.scss"],
 })
 export class ReviewComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
